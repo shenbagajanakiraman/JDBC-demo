da1d78f2772c168889c8caed10fa160f19c55652
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-05-20 10:17:30 +0530

    Updated JDBC Transaction demo code

--- jdbcdemo/src/main/java/com/stackroute/Main.java
+++ jdbcdemo/src/main/java/com/stackroute/Main.java
@@ -1,18 +1,24 @@
 package com.stackroute;
 
 import com.stackroute.db.CrudOperation;
+import com.stackroute.db.JDBCTransactionDemo;
 import com.stackroute.db.ResultsetMetadataDemo;
 import com.stackroute.db.RowsetDemo;
 
 public class Main {
-    public static void main(String[] args) {
-        CrudOperation crudOperation = new CrudOperation();
-        crudOperation.displayData();
+    public static void main(String[] args) throws Exception{
+//        CrudOperation crudOperation = new CrudOperation();
+//        crudOperation.displayData();
 //        crudOperation.displayCustomrerByName("Arjun","Male");
 //        ResultsetMetadataDemo rsDemo = new ResultsetMetadataDemo();
 //        rsDemo.displayData();
 
 //        RowsetDemo rowsetDemo = new RowsetDemo();
 //        rowsetDemo.displayData();
+
+        JDBCTransactionDemo jdbcTransactionDemo = new JDBCTransactionDemo();
+        jdbcTransactionDemo.jdbcTransaction();
+
+
     }
 }
--- jdbcdemo/src/main/java/com/stackroute/db/JDBCTransactionDemo.java
+++ jdbcdemo/src/main/java/com/stackroute/db/JDBCTransactionDemo.java
@@ -1,41 +1,115 @@
 package com.stackroute.db;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class JDBCTransactionDemo {
-    private Connection con;
 
-    public void displayData() {
+    private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/customerdb";
+    private static final String DB_USER = "user";
+    private static final String DB_PASSWORD = "Root@123";
+
+    public void jdbcTransaction() throws SQLException {
+
+        Connection dbConnection = null;
+        PreparedStatement preparedStatementInsert = null;
+        PreparedStatement preparedStatementUpdate = null;
+
+        String insertTableSQL = "INSERT INTO customerdetails"
+                + "(NAME, age, gender) VALUES"
+                + "(?,?,?)";
+
+        String updateTableSQL = "UPDATE customerdetails SET USERNAME =? "
+                + "WHERE USER_ID = ?";
+
         try {
-            //Resister Driver with driver manager service
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbConnection = getDBConnection();
 
-            System.out.println("Driver loaded");//create connection
-            //here db1 is database name, root is username and root123 is password
-            con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/customerdb", "root", "Root@123");
-            //create statement object
-            System.out.println("got connected");
+            dbConnection.setAutoCommit(false);
 
-            PreparedStatement ps = con.prepareStatement("select * from customerdetails");
-            ResultSet rs = ps.executeQuery("Select name,age from customerdetails");
-            ResultSetMetaData rsmd = rs.getMetaData();
+            preparedStatementInsert = dbConnection.prepareStatement(insertTableSQL);
 
-            System.out.println("Total columns: " + rsmd.getColumnCount());
-            System.out.println("Column Name of 1st column: " + rsmd.getColumnName(1));
-            System.out.println("Column Type Name of 1st column: " + rsmd.getColumnTypeName(1));
+            preparedStatementInsert.setString(1, "Prabha");
+            preparedStatementInsert.setInt(2, 45);
+            preparedStatementInsert.setString(3, "Female");
 
+            preparedStatementInsert.executeUpdate();
 
-        } catch (Exception e) {
-            System.out.println(e);
+//            preparedStatementUpdate = dbConnection.prepareStatement(updateTableSQL);
+//            // preparedStatementUpdate.setString(1,
+//            // "A very very long string caused db error");
+//            preparedStatementUpdate.setString(1, "new string");
+//            preparedStatementUpdate.setInt(2, 999);
+//            preparedStatementUpdate.executeUpdate();
+
+            dbConnection.commit();
+
+            System.out.println("Done!");
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+            dbConnection.rollback();
+
         } finally {
-            try {
-                con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+
+            if (preparedStatementInsert != null) {
+                preparedStatementInsert.close();
             }
+
+            if (preparedStatementUpdate != null) {
+                preparedStatementUpdate.close();
+            }
+
+            if (dbConnection != null) {
+                dbConnection.close();
+            }
+
         }
+
     }
+
+    private static Connection getDBConnection() {
+
+        Connection dbConnection = null;
+
+        try {
+
+            Class.forName(DB_DRIVER);
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+        try {
+
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,
+                    DB_PASSWORD);
+            return dbConnection;
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+        return dbConnection;
+
+    }
+
+    private static java.sql.Timestamp getCurrentTimeStamp() {
+
+        java.util.Date today = new java.util.Date();
+        return new java.sql.Timestamp(today.getTime());
+
+    }
+
 }
 
 

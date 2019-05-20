42eb1ea5842e2312b30b292b86c85ca4fca8bebf
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-05-20 11:01:08 +0530

    Updated DataBase MetaData demo code

--- jdbcdemo/src/main/java/com/stackroute/Main.java
+++ jdbcdemo/src/main/java/com/stackroute/Main.java
@@ -4,12 +4,13 @@ import com.stackroute.db.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
+
 //        CrudOperation crudOperation = new CrudOperation();
 //        crudOperation.displayData();
 //        crudOperation.displayCustomrerByName("Arjun","Male");
 //        ResultsetMetadataDemo rsDemo = new ResultsetMetadataDemo();
 //        rsDemo.displayData();
-
+//
 //        RowsetDemo rowsetDemo = new RowsetDemo();
 //        rowsetDemo.displayData();
 
@@ -17,9 +18,11 @@ public class Main {
 //        jdbcTransactionDemo.jdbcTransaction();
 
 
-        JDBCBatchProcessingDemo jdbcBatchProcessingDemo = new JDBCBatchProcessingDemo();
-        jdbcBatchProcessingDemo.jdbcBatchProcessing();
+//        JDBCBatchProcessingDemo jdbcBatchProcessingDemo = new JDBCBatchProcessingDemo();
+//        jdbcBatchProcessingDemo.jdbcBatchProcessing();
 
+        DataBaseMetaDataDemo dataBaseMetaDataDemo = new DataBaseMetaDataDemo();
+        dataBaseMetaDataDemo.metadataDisplay();
 
 
     }
--- jdbcdemo/src/main/java/com/stackroute/db/DataBaseMetaDataDemo.java
+++ jdbcdemo/src/main/java/com/stackroute/db/DataBaseMetaDataDemo.java
@@ -1,4 +1,24 @@
 package com.stackroute.db;
 
+import java.sql.*;
+
 public class DataBaseMetaDataDemo {
-}
+
+        public  void metadataDisplay() {
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                Connection con=DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/customerdb","root","Root@123");
+                DatabaseMetaData dbmd=con.getMetaData();
+
+                System.out.println("Driver Name: "+dbmd.getDriverName());
+                System.out.println("Driver Version: "+dbmd.getDriverVersion());
+                System.out.println("UserName: "+dbmd.getUserName());
+                System.out.println("Database Product Name: "+dbmd.getDatabaseProductName());
+                System.out.println("Database Product Version: "+dbmd.getDatabaseProductVersion());
+
+                con.close();
+            }catch(Exception e){ System.out.println(e);}
+        }
+    }
